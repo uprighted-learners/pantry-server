9ec38a460eec2e85f1969ea120218d7c9463edf7
Missy Eleazer <angelfish814@gmail.com>
2025-06-23 18:46:13 -0400

    built db.js, app.js, .env

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 4000
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const { dbConnect } = require("./config/db")
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+
 
 app.listen(PORT, () => {
+	dbConnect();
 	console.log(`[server] listening on ${PORT}`)
 })
