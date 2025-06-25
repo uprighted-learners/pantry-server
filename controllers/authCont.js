8e1a9e24fe61f85833aed2c27344195cdb570f1c
Missy Eleazer <angelfish814@gmail.com>
2025-06-25 15:33:18 -0400

    finished authCont.js

--- controllers/authCont.js
+++ controllers/authCont.js
@@ -1,31 +1,90 @@
-// ? some sort of express code
-// bcrypt
-
-// mongoose
-// SALT
-// generateToken
-
-// const User = require("../models/User");
-// middleware - isAdmin, authorization, and authentication
+const bcryptjs = require("bcryptjs");
+const User = require("../models/User");
+const SALT = Number(process.env.SALT);
+const { generateToken } = require("../utils/generateToken");
 
 
 
+const registerUser = async (req, res) => {
+    try {
+        const { fullName, zipCode, email, password, isAdmin } = req.body;
+        console.log(fullName, zipCode, email, password, isAdmin);
 
-/* logic code
+        const existingUser = await User.findOne({ "email": email });
+        if (existingUser) {
+            return res.status(409).json({ 
+                message: "A user with this email already exists, please log in."
+            });
+        }
 
-const registerUser = (req, res) => {
-    
+        const hashedPassword = bcryptjs.hashSync(password, SALT);
+
+        const newUser = new User({
+            fullName,
+            zipCode,
+            email,
+            password: hashedPassword,
+            isAdmin: isAdmin || false
+        });
+
+        await newUser.save();
+
+        const token = generateToken(newUser);
+
+        res.status(201).json({
+            message: "User created",
+            user: {
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                isAdmin: newUser.isAdmin
+            },
+            token
+        });
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            error: `${err}` // what exact message does this send and do I need to write one out?
+        });
+    }
 };
 
-const loginUser = (req, res) => {
-    
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ "email": email });
+        if (!user) return res.status(404).json({
+            message: "User not found, please register or check your information and try again."
+        });
+
+        const validPassword = await bcryptjs.compare(password, user.password);
+        if (!validPassword) return res.status(401).json({
+            message: "Invalid entry, please try again"
+        });
+
+        const token = generateToken(user);
+
+        res.status(200).json({
+            message: "Login Successful",
+            user: {
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                isAdmin: user.isAdmin
+            },
+            token
+        });
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: `${err}`
+        });
+    }
 };
 
-?token creation
 
-module.exports = {
-    registerUser,
-    loginUser,
-    
-}
-*/
+
+module.exports = { registerUser, loginUser };
