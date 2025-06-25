3790eabf5b8777fcc7da464797fd47a368a5aaf2
Ronnie Ingram <ringramjr6@gmail.com>
2025-06-25 19:16:09 -0400

    user schema

---
+++ routes/userRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('./models/User');  
+const app = express();
+
+app.post(     , async(req, res)=>{
+    const{fullName, zipCode, email, password, isAdmin} = req.body;
+
+    try{ 
+
+    const newUser = new User ({
+        fullName, 
+        zipCode, 
+        email, 
+        password, 
+        isAdmin: isAdmin || false
+    });
+
+    await newUser.save()
+
+    res.status(200).json({message: 'User registred successfully!'});
+} catch (err) {
+    res.status(500).json({message: 'Error registering user'});
+}
+
+
+});
