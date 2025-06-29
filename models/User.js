3a7505e5d9da4c8fcb601f9bfdf09403b8fc2b29
Ronnie Ingram <ringramjr6@gmail.com>
2025-06-29 07:10:59 -0400

    user model and userRoutes

--- models/User.js
+++ models/User.js
@@ -1,3 +1,32 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema (
+    fullName: {
+        type: String
+        required: true, 
+        maxlength: 100, 
+    }, 
+    zipCode: {
+        type: String, 
+        required: true, 
+        maxlength: 9, 
+    }, 
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        maxlength: 30, 
+    },
+    password: {
+        type: String, 
+        required: true, 
+    },
+    isAdmin: {
+        type: Boolean, 
+        default: false, 
+    }, 
+) 
+module.exports = mongoose.model("user", User);
+
 // const User = require ('../models/User');
 
 // app
