276e775da6f54c68977ba3420629212f2e6168fd
Ronnie Ingram <ringramjr6@gmail.com>
2025-07-01 04:19:49 -0400

    updated Cont.js

---
+++ controllers/userCont.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+
+
+// delete by ID
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: 'Server error', error:  err.message });
+    }
+};
+
+// UPDate by ID
+
+const updateUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error', error: err });
+    }
+};
+
+module.exports = { deleteUser, updateUser};
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const User = require('./models/User');  
-const app = express();
+const router = express.Router();
+const { deleteUser, updateUser } = require('../controllers/userCont');
 
-app.post('', async(req, res)=>{
-    const{fullName, zipCode, email, password, isAdmin} = req.body;
-
-    try{ 
-
-    const newUser = new User ({
-        fullName, 
-        zipCode, 
-        email, 
-        password, 
-        isAdmin: isAdmin || false
-    });
-
-    await newUser.save()
-
-    res.status(200).json({message: 'User registred successfully!'});
-} catch (err) {
-    res.status(500).json({message: 'Error registering user'});
-}
+console.log("Testing Server")
 
 
-});
+//delete
+
+router.delete('/:id', deleteUser);
+
+
+// update 
+
+router.put('/:id', updateUser);
+
+
+
+
+// router.delete('/:id', (req, res) =>{
+//     console.log("potato");
+//     res.send("User is deleted");
+// });
+
+module.exports = router; 
