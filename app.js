eec2bd09c50cbf19cfd4995657beb7efce9cac4e
Ronnie Ingram <ringramjr6@gmail.com>
2025-07-02 18:36:01 -0400

    added user routes to app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const host = "127.0.0.1";
 
 const authRoutes = require("./routes/authRoutes");
 const pantryRoutes = require('./routes/pantryRoutes');
+const userRoutes = requier('./routes/userRoutes');
 
 // testing the routes
 app.get("/", (req, res) => {
@@ -21,6 +22,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes); // There was a ton of confusion around the difference between server and client/html endpoints in my last project, so I looked it up. I couldn't find it using strategic key words, so I asked ChatGPT, but wanted to be sure so I finally just added the exact code -app.use("/api/auth", authRoutes); vs. app.use("/", authRoutes);- to the search engine and that worked. I read this article -https://dev.to/aritik/setting-up-auth-routes-with-express-57oi- This allows for the server/client separation. (Nick asked why, and this is why)
 app.use("/api/pantries", pantryRoutes);
+
+app.use("/api/user", userRoutes);
 
 
 
